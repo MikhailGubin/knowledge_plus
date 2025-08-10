d41d71b59091af83f2f7db231f8e41ec31c61d4e
Mikhail Gubin <mgubin82@gmail.com>
2025-08-10 17:28:40 +0300

    Added ci.yml. Added CI/CD to project Changed after flake8

--- materials/validators.py
+++ materials/validators.py
@@ -10,7 +10,7 @@ def validate_url(value):
 
     print(value)
     if value:
-        reg = re.compile("^(https?://)?(www\.)?youtube\.com/.+$")
+        reg = re.compile(r"^(https?://)?(www\.)?youtube\.com/.+$")
         if not bool(reg.match(value)):
             raise ValidationError(
                 "Использована запрещённая ссылка. Разрешены только ссылки на youtube.com."
--- users/views.py
+++ users/views.py
@@ -11,7 +11,6 @@ from rest_framework.generics import (
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course
 from users.models import CoursePayment, Payment, Subscription, User
